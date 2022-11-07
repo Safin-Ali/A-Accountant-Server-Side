04850cf2bf431d581cece303691551d5dba2db29
Safin-Ali <safin.ali.7205@gmail.com>
2022-11-08 00:05:57 +0600

    server side items installed and imported inside the index file

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// Required Items
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const {} = require('mongodb');
+
