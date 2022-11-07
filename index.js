4c2904c80f9e21696598aae273669db0348815f7
Safin-Ali <safin.ali.7205@gmail.com>
2022-11-08 00:15:30 +0600

    api lisent and port configured

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 
-// Required Items
+// Required Variable
 const express = require('express');
 const cors = require('cors');
+const port = process.env.PORT || 5000;
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const {} = require('mongodb');
 
+// Configure Code
+const app = express();
+dotenv.config();
+
+// Middleware Code
+app.use(cors());
+app.use(express.json());
+
+// Main Part Of A-Accountant ↓↓↓
+
+// get all services
+
+app.get('/',(req,res)=>{
+    res.send('yay i am api')
+})
+
+
+// API Listen Port Calling
+app.listen(port,() => console.log(`this web api running on ${port}`));
